2d6470072e4e9e58917c6979ddb7d5a1e3af7a54
Lucas Salles <lubuntupython@gmail.com>
2025-11-18 17:41:21 -0300

    - Deixamos a classe "CrawlerPinterest' menos acoplada e mais modular, ao permitir a utilização do driver de qualquer navegador suportado pelo SELENIUM.
    - Melhoramos a tipagem do método '__init__' da classe "CrawlerPinterest".

--- src/crawler.py
+++ src/crawler.py
@@ -5,10 +5,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains as AC
 from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.remote.webdriver import WebDriver
 
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import InvalidSelectorException
+
+
 
 import logging
 from utils import configurando_logger
@@ -18,11 +21,11 @@ from traceback import print_exc
 
 #Classes
 
-class CrawlerImagens:
+class CrawlerPinterest:
 
-    def __init__(self,logger:logging.Logger, lista_prompt:list[str]):
+    def __init__(self,driver:WebDriver,logger:logging.Logger,lista_prompt:list[str]):
 
-        self._driver = webdriver.Chrome()
+        self._driver = driver
         self.lista_prompt = lista_prompt
         self.logger = logger
         self._lista_cache= []
@@ -65,8 +68,8 @@ class CrawlerImagens:
         self.logger.debug("[BOT-CRAWLER] Iniciando método 'bot_crawler' para retornar paginas HTML com links de PIN's disponíveis.")
 
         self.logger.info("Entrando no site do Pinterest....")
-        self.logger.debug("[BOT-CRAWLER] Método 'crawler_main' iniciado, iniciando itereção de valores da lista" \
-        "'self.lista_prompt'")
+        self.logger.debug("[BOT-CRAWLER] Método 'bot_crawler' iniciado. Iniciando a iteração dos valores da lista" \
+        " 'self.lista_prompt'")
 
         for prompt in self.lista_prompt:
             #Entrando no site e achando o input de pesquisa
@@ -89,7 +92,7 @@ class CrawlerImagens:
                     #Veririfcando se a quantidade bate com a que foi requisitada
                     if len(lista_div_img) < max_img:
                         self.logger.info(f"\nAchamos apenas {len(lista_div_img)} imagens para o prompt => {prompt}")
-                        self.logger.info("Vamos procurar mais....\n")
+                        self.logger.info("Vamos procurar mais....")
 
                         #DEBUG
                         time.sleep(4)
@@ -99,15 +102,15 @@ class CrawlerImagens:
                     
                     else:
                         #Salvando o HTML estatico no dicionario 'dict_pagina_html' tendo a chave como prompt
-                        self.logger.debug(f"[BOT-CRAWLER] Achamos {len(lista_div_img)} imagens da requisição de {max_img} imagens.")
-                        self.logger.info(f"\nAchamos todas as imagens! Salvando a pagina do prompt => {prompt}")
+                        self.logger.debug(f"\n[BOT-CRAWLER] Achamos {len(lista_div_img)} imagens da requisição de {max_img} imagens.")
+                        self.logger.info(f"Achamos todas as imagens! Salvando a pagina do prompt => {prompt}")
                         dict_pagina_html[prompt] = self.driver.page_source
                         break
                 
                 except TimeoutException as error:
                     #Tratando o problema do bloco de login "congelando" a página
-                    self.logger.debug(f"[BOT-CRAWLER] Exceção 'TimeoutException' levantada com o prompt => {prompt}")
-                    self.logger.info(f"\nBloco de login apareceu no prompt => {prompt}")
+                    self.logger.debug(f"\n[BOT-CRAWLER] Exceção 'TimeoutException' levantada com o prompt => {prompt}")
+                    self.logger.info(f"Bloco de login apareceu no prompt => {prompt}")
                     self.logger.info(f"Fechando ele para continuar com o fluxo...")
 
                     time.sleep(4)
@@ -115,8 +118,8 @@ class CrawlerImagens:
                     self.verifica_bloco_login()
         
         #Retornando dicionario com as paginas HTML
-        self.logger.debug("[BOT-CRAWLER] Iteração de todos os prompts terminada, retornando o dicionario 'dict_pagina_html'.")
-        self.logger.info("\nCaptura das páginas terminada!")
+        self.logger.debug("\n[BOT-CRAWLER] Iteração de todos os prompts terminada, retornando o dicionario 'dict_pagina_html'.")
+        self.logger.info("Captura das páginas terminada!")
         return dict_pagina_html
                         
     def verifica_bloco_login(self) -> bool:
@@ -144,25 +147,30 @@ class CrawlerImagens:
                 return True
         
         except InvalidSelectorException as error:
-            self.logger.info("Bloco Login não encontrado! Erro no programa!")
+
+            #Problema grave. Algo esta interrompendo o fluxo e precisa ser consertado
+            #Fazendo limpeza e encerrando programa
+            self.driver.quit()
+
+            self.logger.info("Bloco Login não encontrado! Erro grave no programa! De uma olhada no log de erro 'Error.log'!")
             self.logger.error(f"[BOT-CRAWLER] Bloco login não encontrado." /
                               f"Outra coisa não esta deixando o CrawlerPinterest encontrar as imagens. => {error}\n{print_exc()}")
             
-            return False
-
+            raise InvalidSelectorException
+        
 
 #Função Main para DEBUG
 
 def main():
 
-    #Testando instancia do CrawlerImagens e geração de bots com uma Queue
-    mock_lista_prompt = ["Lucy Heartfilia", "Nami", "Zelda"] 
+    #Testando instancia do CrawlerImagens na captura de páginas HTML
+    mock_lista_prompt = ["Lucy Heartfilia", "Nami", "Zelda", "Android 18"] 
     logger = configurando_logger(debug_mode=True)
-    crawler = CrawlerImagens(logger,mock_lista_prompt)
-    crawler.bot_crawler()
-    
-    crawler.driver.quit()
+    driver = webdriver.Chrome()
 
+    crawler = CrawlerPinterest(driver,logger,mock_lista_prompt)
+    crawler.bot_crawler()
+    crawler.driver.quit()
 
 
 if __name__ == "__main__":
