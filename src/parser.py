ca10eb0d9a955daa25c87f4c10a41eec5d432760
Lucas Salles <lubuntupython@gmail.com>
2025-11-27 16:43:37 -0300

    - Implementando tratamento de exceçao caso o desenvolvedor forneça um dicionário vazio a instancia da classe 'ParserHTMLPinterest'.

--- src/parser.py
+++ src/parser.py
@@ -1,11 +1,43 @@
+"""
+Módulo responsável por disponibilizar parsers de páginas HTML para diferentes sites.
+
+Atualmente, este módulo fornece a classe `ParserImagensPinterest`, que implementa
+as funcionalidades necessárias para realizar a requisição da pagina HTML dos links 
+que contem as imagens dos pins, e a retirada de todo os links das imagens, que são colocados
+em um dicionários, que leva como chave para cada lista de links, o prompt que a gerou. 
+
+Dependências:
+    - bs4
+    - aiohttp
+    - utils.py (módulo interno desta aplicação)
+
+Exemplo:
+    
+
+Notas:
+    Este módulo não deve ser executado diretamente, utilize ele apenas via 'import'.
+
+"""
+
+
 from bs4 import BeautifulSoup
-import requests
+import aiohttp
 from abc import ABC,abstractmethod
+import logging
 
 
 #Classe Abstrata
 
-class ParserImagens(ABC):
+class ParserHTML(ABC):
+
+    """
+    Classe base abstrata para parsers de diferentes sites.
+
+    Esta classe define a interface mínima que todo parser deve implementar,
+    garantindo um comportamento consistente entre diversas plataformas.
+    Subclasses devem sobrescrever os métodos abstratos para fornecer a
+    lógica específica de cada site.
+    """
     
     @abstractmethod
     async def bot_requisicao(self):
@@ -18,15 +50,22 @@ class ParserImagens(ABC):
 
 #Sub-Classes da Abstrata
 
-class ParserImagensPinterest(ParserImagens):
+class ParserHTMLPinterest(ParserHTML):
 
-    def __init__(self):
+    def __init__(self, dict_links_html:dict[str:str], logger:logging.Logger):
+
+        self.dict_links_html = dict_links_html
+        self.logger = logger
+
+        self.logger.debug(f"[INIT - ParserPinterest] Verificando se o dicionário passado para 'dict_links_html' esta vazio.")
+        if not self.dict_links_html:
+            self.logger.debug(f"[INIT - ParserPinterest] O dicionário fornecido esta vazio! Levantando exceção e encerrando o programa!")
+            raise ValueError("O valor do argumento 'dict_links_html' não pode estar vazio!")
+
+    async def bot_requisicao(self):
         pass
 
-    def bot_requisicao(self):
-        pass
-
-    def bot_parser(self):
+    async def bot_parser(self):
         pass
 
 
