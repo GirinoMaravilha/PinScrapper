86fd1c52e2341a1ca1648839531d292c983d8828
Lucas Salles <lubuntupython@gmail.com>
2025-12-03 18:44:56 -0300

    - FInalização completa do método '_bot_salva_imagens'.
    - Criado método '_salva_imagens' para facilitar o processo de registro dos arquivos de imagem no SO pelo método '_bot_salva_imagens'.
    
    - Criado lista 'TODO' no começo do módulo, especificando tarefas que ainda faltam serem completadas para o termino completo dessa fase do projeto.
    
    - Código para depuração completa das funcionalidades da classe 'Downloader' criado na função 'main'.

--- src/downloader.py
+++ src/downloader.py
@@ -19,12 +19,20 @@ Notas:
 
 """
 
+#TODO Tarefas que ainda faltam:
+#- Fazer a documentação do módulo
+#- Fazer a documentação dos métodos e classes
+#- Fazer o encapsulamento dos atributos
+#- Verificar se existem mais exceções para serem tratadas
+
 import asyncio
 import aiohttp
 from pathlib import Path
 import logging
 from utils import configurando_logger
 from utils import salva_imagem
+from pathlib import Path
+import time
 
 
 # Classes
@@ -78,7 +86,7 @@ class Downloader:
         for prompt,lista_img in self._dict_lista_links.items():
             n_req += 1
             lista_task_requisicao.append(asyncio.create_task(self._bot_requisicao(n_req,prompt,lista_img,fila,evento,semaforo,lock)))
-        lista_task_salva_imagens = [asyncio.create_task(self._bot_salva_imagens()) for n in range(len(self._dict_lista_links))]
+        lista_task_salva_imagens = [asyncio.create_task(self._bot_salva_imagens(n+1,fila,evento)) for n in range(len(self._dict_lista_links))]
 
         #Chamando o método 'join' para esperar fim da pipeline antes de seguir com o fluxo
         self.logger.debug("\n[DOWNLOADING] - Método 'join' chamado para 'travar' o método 'downloading' até o fluxo da pipeline ser finalizado.")
@@ -90,7 +98,7 @@ class Downloader:
 
         self.logger.debug("[DOWNLOADING] - Bots de requisição e salvar imagens finalizados! Encerrando o programa!")
 
-    async def _bot_requisicao(self,numero_id:int,prompt:str,lista_links_img:list[str],fila:asyncio.Queue,evento:asyncio.Event, semaforo:asyncio.Semaphore, lock:asyncio.Lock):
+    async def _bot_requisicao(self,numero_id:int,prompt:str,lista_links_img:list[str],fila:asyncio.Queue,evento:asyncio.Event, semaforo:asyncio.Semaphore, lock:asyncio.Lock) -> None:
 
         ### Variáveis ###
 
@@ -127,7 +135,7 @@ class Downloader:
                                 img_io = await resp.read()
 
                                 #DEBUG para testar se estamos pegando bytes das imagens corretamente
-                                salva_imagem(img_io)
+                                #salva_imagem(img_io)
 
                                 self.logger.debug(f"[BOT_REQUISICAO - {numero_id}] Requisição bem sucedida! Armazenando bytes do link => {link} - na lista 'lista_img_bytes'")
 
@@ -152,7 +160,7 @@ class Downloader:
         self.logger.debug(f"[BOT_REQUISICAO - {numero_id}] Inserindo tupla com o prompt e a lista de bytes 'lista_img_bytes' na pipeline")
         await fila.put((prompt, lista_img_bytes)) 
 
-        self.logger.info(f"Download das imagens do prompt => {prompt} - Finalizado!")
+        self.logger.info(f"\nDownload das imagens do prompt => {prompt} - Finalizado!")
 
         #Sinalizando introdução na pipeline e verificando se pode ativar a flag do 'Event'
         self.logger.debug(f"[BOT_REQUISICAO - {numero_id}] Sinalizando fim da produção e verificando o numero de produtores ativos, caso não haja nenhum, ativando flag do 'Event'")
@@ -162,8 +170,83 @@ class Downloader:
                 self.logger.debug(f"[BOT_REQUISICAO - {numero_id}] Todos os produtores terminaram! Ativando a flag do 'Event'.")
                 evento.set()
 
-    async def _bot_salva_imagens(self):
-        pass
+    async def _bot_salva_imagens(self, numero_id:int, fila:asyncio.Queue, evento:asyncio.Event) -> None:
+        
+        ### Variáveis ###
+
+        #Valor do prompt e lista de bytes de imagens do mesmo
+        prompt = ""
+        lista_bytes_img = []
+
+        ### Código ###
+
+        self.logger.debug(f"[BOT_SALVA_IMG - {numero_id}] Bot Iniciado!")
+ 
+        #Iniciando a iteração de produção
+        while True:
+            #Verificando se a produção esta finalizada
+            if fila.empty() and evento.is_set():
+                self.logger.debug(f"[BOT_SALVA_IMG - {numero_id}] Pipeline vazia! Flag do 'Event' esta ativada! Produção finalizada, encerrando o bot...")
+                break
+            
+            #Retirando e processando valor da pipeline
+            self.logger.debug(f"[BOT_SALVA_IMG - {numero_id}] Tentando retirar valor da pipeline")
+            try:
+                prompt,lista_bytes_img = await asyncio.wait_for(fila.get(),5)
+                self.logger.debug(f"[BOT_SALVA_IMG - {numero_id}] Valor retirado com sucesso!")
+
+                #Salvando list de imagens em um diretório que leva o nome do prompt
+                self.logger.info(f"Salvando imagens do prompt => {prompt} - no computador....")
+                self.logger.debug(f"[BOT_SALVA_IMG - {numero_id}] Criando diretório com o nome do prompt => {prompt} e salvando imagens nele")
+                self._salva_imagens(prompt,lista_bytes_img)
+
+                #Sinalizando que um item da pipeline ja foi processado
+                self.logger.debug(f"[BOT_SALVA_IMG - {numero_id}] Imagens dos prompt '{prompt}' salvas! Sinalizando para a pipeline que um item ja foi processado")
+                self.logger.info(f"Registro no computador das Imagens do prompt => '{prompt}' - finalizados!")
+                fila.task_done()
+
+            except asyncio.TimeoutError as error:
+                self.logger.debug(f"[BOT_SALVA_IMG - {numero_id}] Bot demorou demais para retirar valor da pipeline! Seguindo o fluxo...")
+                continue
+    
+    def _salva_imagens(self, prompt:str, lista_bytes_img:list[str]) -> None:
+
+        ### Variáveis ###
+
+        #Instancia Path
+        path = None
+
+        #Valor do tempo atual duranto a execução do programa
+        data = ""
+        hora = ""
+
+        #Número da imagem
+        n_img = 0
+
+        ### Código ###
+
+        #Capturando tempo atual
+        data = time.strftime(fr"%d %m %Y",time.localtime())
+        hora = time.strftime(fr"%H %M %S",time.localtime())
+
+        #Iniciando instancia Path e criando diretório Principal
+        path = Path(f"Imagens_Pinterest - {data}")
+        path = path / prompt
+        
+        #Tentando criar um diretório com o valor de 'prompt'
+        try:
+            path.mkdir(exist_ok=True, parents=True)
+        
+        except (ValueError,OSError) as error:
+            path = Path(f"Imagens_Pinterest - {data}")
+            path = path / f"Captura de Imagens {hora}"
+            path.mkdir(exist_ok=True, parents=True)
+        
+        #Iterando valores da 'lista_bytes_img' para salvar as imagens
+        for img_bytes in lista_bytes_img:
+            n_img += 1
+            with open(f"{path}/img{n_img}.jpg","wb") as img:
+                img.write(img_bytes)
 
 
 #Função Main para Depuração
@@ -174,9 +257,21 @@ def main():
                                              'https://i.pinimg.com/736x/96/b1/34/96b134d9a605eac02a1a1693137b3974.jpg', 'https://i.pinimg.com/736x/39/c5/ac/39c5ac79e3782ff576ad3f6a6b4351f2.jpg', 
                                              'https://i.pinimg.com/736x/3f/e7/d1/3fe7d1fa3a4ad1178ead57451f7a52df.jpg', 'https://i.pinimg.com/736x/6b/72/cb/6b72cbfb5e7600b783b6b76055b803fd.jpg', 
                                              'https://i.pinimg.com/736x/14/99/c6/1499c6640b438ab19bf875fca1a96b39.jpg', 'https://i.pinimg.com/736x/e0/fb/6a/e0fb6ad2da0893929b85c51b28f79001.jpg', 
-                                             'https://i.pinimg.com/736x/a0/dc/d7/a0dcd7433bf03afc4ceb9beadce63716.jpg', 'https://i.pinimg.com/736x/fb/15/f3/fb15f35b662608853cca87e2e8347d9c.jpg']}
+                                             'https://i.pinimg.com/736x/a0/dc/d7/a0dcd7433bf03afc4ceb9beadce63716.jpg', 'https://i.pinimg.com/736x/fb/15/f3/fb15f35b662608853cca87e2e8347d9c.jpg'],
+
+                      'Nami One Piece': ['https://i.pinimg.com/736x/ae/60/76/ae607673ca5a12fa09c795cb5abc5451.jpg', 'https://i.pinimg.com/736x/19/b6/76/19b67645a6d0956c69dc4ceeebc8d04f.jpg', 
+                                         'https://i.pinimg.com/736x/8f/23/19/8f23190beada3d1f54cc39d0515d1dc8.jpg', 'https://i.pinimg.com/736x/19/82/a6/1982a6184011e41357552a98242da639.jpg', 
+                                         'https://i.pinimg.com/736x/c0/cd/c3/c0cdc34756a9a4d7459ccb2d6c3ee023.jpg', 'https://i.pinimg.com/736x/e2/9d/8e/e29d8e0b2099b748672227813d95e29f.jpg', 
+                                         'https://i.pinimg.com/736x/cf/67/c5/cf67c58400be876cad95f47b687b28ea.jpg', 'https://i.pinimg.com/736x/9b/7b/46/9b7b461a259f0862c758840403bb9202.jpg', 
+                                         'https://i.pinimg.com/736x/c7/e4/69/c7e46919f4f4937b9fbc000982ab913a.jpg', 'https://i.pinimg.com/736x/10/45/6a/10456a9bc13a5ca5822c45b61e5d6571.jpg'], 
+
+                      'Lucy Heatfilia': ['https://i.pinimg.com/736x/35/63/e2/3563e2ffd807a83dff9f7a29ca78c90c.jpg', 'https://i.pinimg.com/736x/66/f8/42/66f842cd3eb5817e4c99eeaf73cdb4bf.jpg', 
+                                         'https://i.pinimg.com/736x/cc/61/dd/cc61dd6dfe86ebf4a4f2f4578f7edf15.jpg', 'https://i.pinimg.com/736x/8d/18/5f/8d185fee68d0364280918b6e213e3665.jpg', 
+                                         'https://i.pinimg.com/736x/fd/ec/96/fdec969f9aa0fedc5cd75e3f9415a3f0.jpg', 'https://i.pinimg.com/736x/14/ad/87/14ad87e48ba41e837511f0a4707ed109.jpg', 
+                                         'https://i.pinimg.com/736x/67/77/4e/67774e9bf2f30e5197e894db3d55ec98.jpg', 'https://i.pinimg.com/736x/0b/cb/4a/0bcb4a0b80219c2cf07c4181cfdad6b1.jpg', 
+                                         'https://i.pinimg.com/736x/b3/96/4b/b3964b9f9d3ca4aa41d29428e167ce5b.jpg', 'https://i.pinimg.com/736x/d7/25/72/d72572ae4a7314f5172ea194139449ac.jpg']}
     
-    logger = configurando_logger(debug_mode=True)
+    logger = configurando_logger(debug_mode=False)
     d = Downloader(logger,dict_links_img)
 
     #Testando instancia de Downloader
