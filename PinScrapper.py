e25ceb9895a7fbf45d4d023f926eaf6976bf0c01
Lucas Salles <lubuntupython@gmail.com>
2025-12-04 18:45:05 -0300

    - Classes abstratas dos módulos do pacote 'src' importadas.
    - Código do método 'principal' da classe 'PinScrapper' modificado para deixa-lo mais modular e com baixo acoplamento, no caso recebendo instancias do crawler,parser e downloader por injeção de dependencia.

--- PinScrapper.py
+++ PinScrapper.py
@@ -9,8 +9,8 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 #from selenium.webdriver.safari.options import Options as SafariOptions
 from selenium.webdriver.remote.webdriver import WebDriver
 
-from src.crawler import CrawlerPinterest
-from src.parser import ParserHTMLPinterest
+from src.crawler import CrawlerPinterest,Crawler
+from src.parser import ParserHTMLPinterest, ParserHTML
 from src.downloader import Downloader
 from src.utils import configurando_logger
 import argparse
@@ -28,18 +28,18 @@ class PinScrapper:
         self.driver = driver
         self.max_img = max_img
 
-    def principal(self):
+    def principal(self, crawler:Crawler, parser:ParserHTML, downloader:Downloader):
         
         ### Variáveis ###
 
         #Instancia da classe 'CrawlerPinterest'
-        crawler = None
+        c = None
 
         #Instancia da classe 'ParserHTMLPinterest
-        parser = None
+        p = None
 
         #Instancia da classe 'Downloader'
-        download = None
+        d = None
 
         #Dicionário contendo listas de links de pins gerados por prompts especificos
         dict_lista_links_pin = {}
@@ -52,16 +52,16 @@ class PinScrapper:
         self.logger.debug("[PRINCIPAL] Método principal iniciado!")
 
         #Iniciando instancia do crawler e chamando método para conseguir os links de cada pin
-        crawler = CrawlerPinterest(self.driver,self.logger,self.lista_prompt)
-        dict_lista_links_pin = crawler.bot_crawler(max_img=self.max_img)
+        c = crawler(self.driver,self.logger,self.lista_prompt)
+        dict_lista_links_pin = c.bot_crawler(max_img=self.max_img)
 
         #Iniciando instancia do parser e chamando métodos assíncrono para conseguir os links de cada imagem
-        parser = ParserHTMLPinterest(dict_lista_links_pin,self.logger)
-        dict_lista_links_img = asyncio.run(parser.parsing())
+        p = parser(dict_lista_links_pin,self.logger)
+        dict_lista_links_img = asyncio.run(p.parsing())
 
         #Iniciando instancio do downloades e chamando método assincrono para baixar todas as imagens e salva-las no SO
-        download = Downloader(self.logger, dict_lista_links_img)
-        asyncio.run(download.downloading())
+        d = downloader(self.logger, dict_lista_links_img)
+        asyncio.run(d.downloading())
 
 
 #Função Main
